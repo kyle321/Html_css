b8570baf0116d48bcc5cfb1aa5aafa49b231e132
mykyle <myk2030@gmail.com>
2017-07-18 14:59:25 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML and Css</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+	<header>
+		<a href="" id="logo"></a>
+		<nav>
+			<a href="" id="menu-icon"></a>
+			<ul>
+				<li><a href="" class="current">Home</a></li>
+				<li><a href="">About us</a></li>
+				<li><a href="">Gallery</a></li>
+				<li><a href="">History</a></li>
+			</ul>
+		</nav>
+	</header>
+	<div class="clear"></div>
+	<section>
+		<h1>About us</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</section>
+	<section>
+		<h1>Our services</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</section>
+	<section>
+		<h1>Our Clients</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</section>
+	<div class="clear"></div>
+	<article>
+		<h1>History</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</article>
+	<aside>
+		<h1>Portifolio</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</aside>
+	<div class="clear"></div>
+	<footer>
+		<section>
+			<h1>Contact Info</h1>
+			<h2>ParkLand Avenue</h2>
+			<p>
+				<strong>0712744255</strong><br>
+				myk2030@gmail.com<br>
+				+254 222 555 122
+			</p>
+		</section>
+		<section>
+			<h2>Follow Us</h2>
+			<ul>
+				<li><img src="images/twitter.png" width="50"></li>
+				<li><img src="images/fb.png" width="50"></li>
+				<li><img src="images/linked.jpg" width="50"></li>
+			</ul>
+		</section>
+		<section>
+			<h2>Logo</h2>
+		</section>
+	</footer>
+	<div class="clear"></div>
+	<footer>
+		<h1>&copy copyright 2017</h1>
+	</footer>
+	
+</body>
+</html>
